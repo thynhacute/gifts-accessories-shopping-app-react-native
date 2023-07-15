2e25e06271fc41d4cfcb9d1f905a1f4cb7ce5988
thynhacute <nhathy07@gmail.com>
2023-07-16 06:19:09 +0700

    update colors

--- src/color.js
+++ src/color.js
@@ -6,12 +6,14 @@ const Colors = {
   orange: "#FFCE31",
   red: "#FF0000",
   gray: "#F4F4F4",
-  subGreen: "#EEFAE6",
+  subGreen: "#FFCCCC",
   deepGray: "#E1F0D7",
   deepestGray: "#C4C4C4",
   blue: "#3B87F9",
   underline: "#E5E5E5",
   paypal: "#FFB730",
+  pink: "#FFC0CB",
+  blue: "00008B",
 };
 
 export default Colors;
