79913ca20e2037658bc7e6d099848f8f9278d67b
thynhacute <nhathy07@gmail.com>
2023-07-16 06:21:58 +0700

    add favorite-page

---
+++ src/Screens/FavoriteScreen.js
@@ -0,0 +1,13 @@
+import { useNavigation } from "@react-navigation/native";
+import { Box, Button, Center, HStack, ScrollView, Text } from "native-base";
+import React from "react";
+import Colors from "../color";
+import Buttone from "../Components/Buttone";
+// import FavoriteEmpty from "../Components/FavoriteEmpty";
+
+function FavoriteScreen() {
+  const navigation = useNavigation();
+  return <Text>Phây vo rịt</Text>;
+}
+
+export default FavoriteScreen;
