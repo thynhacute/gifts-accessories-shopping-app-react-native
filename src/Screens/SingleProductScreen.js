e3b6b2fa1aecea41406d25e25f4842546b291340
Hoangtammht <hoangtammht@gmail.com>
2023-07-16 21:59:03 +0700

    Cart Detail Function

--- src/Screens/SingleProductScreen.js
+++ src/Screens/SingleProductScreen.js
@@ -15,11 +15,33 @@ import NumericInput from "react-native-numeric-input";
 import Buttone from "../Components/Buttone";
 import Review from "../Components/Review";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 function SingleProductScreen({ route }) {
-  const [value, setValue] = useState(0);
+  const [quantity, setQuantity] = useState(1);
   const navigation = useNavigation();
   const product = route.params;
+
+  const addToCart = async () => {
+    const cartItem = {
+      product,
+      quantity,
+    };
+    try {
+      const cartItems = await AsyncStorage.getItem("cartItems");
+      if (cartItems) {
+        const parsedCartItems = JSON.parse(cartItems);
+        const updatedCartItems = [...parsedCartItems, cartItem];
+        await AsyncStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
+      } else {
+        await AsyncStorage.setItem("cartItems", JSON.stringify([cartItem]));
+      }
+    } catch (error) {
+      console.log("Error adding item to cart:", error);
+    }
+    navigation.navigate("Cart", { item: cartItem });
+  };
+
   return (
     <Box safeArea flex={1} bg={Colors.white}>
       <ScrollView px={5} showsVerticalScrollIndicator={false}>
@@ -37,7 +59,8 @@ function SingleProductScreen({ route }) {
         <HStack space={2} alignItems="center" my={5}>
           {product.countInStock > 0 ? (
             <NumericInput
-              value={value}
+              value={quantity}
+              onChange={setQuantity}
               totalWidth={140}
               totalHeight={30}
               iconSize={25}
@@ -69,7 +92,7 @@ function SingleProductScreen({ route }) {
           Inventory: {product.countInStock}
         </Text>
         <Buttone
-          onPress={() => navigation.navigate("Cart")}
+          onPress={addToCart}
           bg={Colors.main}
           color={Colors.white}
           mt={10}
